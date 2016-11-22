bff1bfc312e39068ba1d053888ce4b94328ebb13
Jodok Batlogg <jodok@batlogg.com>
2016-11-22 10:38:14 +0100

    remove pid file environment variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,7 +18,6 @@ if [ "$1" = 'mosquitto' ]; then
     : ${MOSQUITTO_LOG_DEST=stdout}
     : ${MOSQUITTO_PERSISTENCE=true}
     : ${MOSQUITTO_PERSISTENCE_LOCATION=/data}
-    : ${MOSQUITTO_PID_FILE=/var/run/mosquitto.pid}
 
     # if no configfile is provided, generate one based on the environment variables
     if [ ! -f /config/mosquitto.conf ]; then
@@ -27,7 +26,6 @@ if [ "$1" = 'mosquitto' ]; then
         sed -e "s#log_dest syslog#log_dest $MOSQUITTO_LOG_DEST#" \
             -e "s#\#persistence false#persistence $MOSQUITTO_PERSISTENCE#" \
             -e "s#\#persistence_location#persistence_location $MOSQUITTO_PERSISTENCE_LOCATION#" \
-            -e "s#\#pid_file#pid_file $MOSQUITTO_PID_FILE#" \
             \
             /config/mosquitto.conf.dist > /config/mosquitto.conf
     fi
